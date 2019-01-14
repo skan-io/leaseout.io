3f6ac773d7800adda4cc4b47ffa8d7d540acf653
nickmanks <nick.manks@gmail.com>
2019-01-14 13:42:07 +1000

    Landing footer

--- src/footers/landing/index.js
+++ src/footers/landing/index.js
@@ -3,27 +3,31 @@ import './theme.scss';
 
 
 const LandingFooter = ()=> (
-    <footer className="py-5 landing-footer" id="footer-main">
+  <footer className="py-5 landing-footer" id="footer-main">
     <div className="container">
       <div className="row align-items-center justify-content-xl-between">
         <div className="col-xl-6">
           <div className="copyright text-center text-xl-left text-muted">
-            © 2019 <a href="https://www.creative-tim.com" className="font-weight-bold ml-1" target="_blank">LeasePlease.com</a>
+            © 2019 <a href="/" className="font-weight-bold ml-1">
+            LeasePlease.com
+            </a>
           </div>
         </div>
         <div className="col-xl-6">
-          <ul className="nav nav-footer justify-content-center justify-content-xl-end">
+          <ul
+            className="nav nav-footer justify-content-center justify-content-xl-end"
+          >
             <li className="nav-item">
-              <a href="https://www.creative-tim.com" className="nav-link" target="_blank">Support</a>
+              <a href="/support" className="nav-link">Support</a>
             </li>
             <li className="nav-item">
-              <a href="https://www.creative-tim.com/presentation" className="nav-link" target="_blank">About Us</a>
+              <a href="/about" className="nav-link">About Us</a>
             </li>
             <li className="nav-item">
-              <a href="http://blog.creative-tim.com" className="nav-link" target="_blank">API Integrations</a>
+              <a href="/api" className="nav-link">API Integrations</a>
             </li>
             <li className="nav-item">
-              <a href="https://www.creative-tim.com/license" className="nav-link" target="_blank">Tenancy Resources</a>
+              <a href="/resources" className="nav-link">Tenancy Resources</a>
             </li>
           </ul>
         </div>
